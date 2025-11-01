f5ab800aee074180ff4bb640ff9f2aa51fc209f8
bharatverma <bharatbigj@gmail.com>
2025-11-01 10:04:58 +0530

    Time and Frequency Measure.

--- TimingMeasure/time_measure.c
+++ TimingMeasure/time_measure.c
@@ -1,3 +1,10 @@
+/*
+ * TimingMeasure/time_measure.c
+ *
+ * Measure time intervals using Timer0 and external interrupts on an 8051 microcontroller.
+ * The timer counts milliseconds, and the external interrupt toggles the timer's operation.
+ * The elapsed time in seconds is calculated and displayed on port P2.
+ */
 #include <reg51.h>
 
 unsigned long time_ms = 0;
@@ -16,10 +23,9 @@ void init_timer()
     TF0 = 0;
     TR0 = 0;
     ET0 = 1; // Enable Timer0 interrupt (vector provided by timer0_isr)
-    IT0 = 1; // INT0 edge-triggered (falling edge) for clean toggling
     TH0 = 0xFC;
     TL0 = 0x18;
-    time_ms = 0;
+    time_ms = 0; // Reset time counter
 }
 
 void ext0_isr(void) interrupt 0
@@ -39,6 +45,7 @@ void main()
     float f;
     init_timer();
     EX0 = 1; // Enable external interrupt 0
+    IT0 = 1; // INT0 edge-triggered (falling edge) for clean toggling
     EA = 1;  // Enable global interrupts
 
     while (1)
